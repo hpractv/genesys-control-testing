7627b3509e82062f815a30f769c846b0dae4f6fb
Roy <hpractv@groundworksdesign.com>
2025-02-05 15:30:42 -0700

    Save Point: working on Search

--- library.js
+++ library.js
@@ -12,7 +12,23 @@ gct.getQueryParam = name => {
   return gct.urlParams.has(name) ? gct.urlParams.get(name) : null;
 };
 
-gct.init = () => {
+const loadData = async () => {
+  await fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      gct.population = data.population;
+    })
+    .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+    });
+};
+
+gct.init = async () => {
   gct.urlParams = new URLSearchParams(window.location.search);
   gct.params = {};
   gct.params.langTag = gct.getQueryParam('langTag');
@@ -20,4 +36,6 @@ gct.init = () => {
   gct.params.gcHostOrigin = gct.getQueryParam('gcHostOrigin');
   gct.params.conversationId = gct.getQueryParam('conversationId');
   gct.params.usePopupAuth = gct.getQueryParam('usePopupAuth');
+
+  await loadData();
 };
